055b428320e1b20446dac0088f4da35d76a5b52d
lijinc <lijinc100@gmail.com>
2014-04-09 00:04:48 +0530

    my first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#11 SMP Mon Apr 7 12:51:43 IST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "lij-Inspiron-N5110"
+#define LINUX_COMPILER "gcc version 4.8.1 (Ubuntu/Linaro 4.8.1-10ubuntu9) "
